a8a8b400d0e7dac563a7cca357d8ee533b3ebfd2
Sunitha Mekala <sunitha.mekala58@gmail.com>
2025-01-08 23:50:49 +0530

    Update segment_test.py

--- YOLO V11-BRAIN TUMOR/segment_test.py
+++ YOLO V11-BRAIN TUMOR/segment_test.py
@@ -4,6 +4,8 @@ from ultralytics import YOLO
 
 model = YOLO("C:\\Users\\SUNITHA\\A VS CODE\\runs\\segment\\train\\weights\\best.pt")
 
-results = model("C:\\Users\\SUNITHA\\A VS CODE\\PYDEVCODE\\BRAIN-TUMOR11SEG\\test_images", save = True)
+results = model("C:\\Users\\SUNITHA\\A VS CODE\\PYDEVCODE\\BRAIN-TUMOR11SEG\\test_images\1.jpg", save = True)
+#results = model("C:\\Users\\SUNITHA\\A VS CODE\\PYDEVCODE\\BRAIN-TUMOR11SEG\\test_images\2.jpg", save = True)
+#results = model("C:\\Users\\SUNITHA\\A VS CODE\\PYDEVCODE\\BRAIN-TUMOR11SEG\\test_images\3.jpg", save = True)
 
 #results[0].show()
